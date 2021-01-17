b7fd360c465d70ebb3777dae1b4203a9ecf5ef4c
Ethan Johnson <ethan@ejohnsons.com>
2021-01-16 18:39:02 -0600

    Initial repo setup

---
+++ src/main/java/dev/lazurite/corduroy/Corduroy.java
@@ -0,0 +1,10 @@
+package dev.lazurite.corduroy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Corduroy implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
